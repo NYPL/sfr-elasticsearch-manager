fe39dd1b805faeeba079f657a1fc6daaf77cc727
Mike Benowitz <mwbenowitz@gmail.com>
2020-01-07 16:54:52 -0500

    SFR-591 Fix db manager mocks

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -1,37 +1,42 @@
 import unittest
-from unittest.mock import patch, call, MagicMock
+from unittest.mock import patch, call, MagicMock, DEFAULT
 import os
+from sfrCore import SessionManager
 
 from helpers.errorHelpers import ESError
 
 os.environ['DB_USER'] = 'test'
-os.environ['DB_PASS'] = 'test'
+os.environ['DB_PSWD'] = 'test'
 os.environ['DB_HOST'] = 'test'
 os.environ['DB_PORT'] = '1'
 os.environ['DB_NAME'] = 'test'
-os.environ['ES_INDEX'] = 'test'
-
-# This method is invoked outside of the main handler method as this allows
-# us to re-use db connections across Lambda invocations, but it requires a
-# little testing weirdness, e.g. we need to mock it on import to prevent errors
-with patch('service.SessionManager') as mock_db:
-    from service import handler, indexRecords, IndexingManager
 
 
 class TestHandler(unittest.TestCase):
+    @patch.multiple(
+        SessionManager, generateEngine=DEFAULT, decryptEnvVar=DEFAULT
+    )
+    def setUp(self, generateEngine, decryptEnvVar):
+        from service import handler, indexRecords, IndexingManager
+        self.handler = handler
+        self.indexRecords = indexRecords
+        self.indexingManager = IndexingManager
 
     @patch('service.indexRecords', return_value=True)
     def test_handler_clean(self, mock_index):
         testRec = {
             'source': 'CloudWatch'
         }
-        resp = handler(testRec, None)
+        resp = self.handler(testRec, None)
         mock_index.assert_called_once()
         self.assertTrue(resp)
 
-    @patch.object(IndexingManager, 'loadUpdates')
     @patch('lib.esManager.createAWSClient')
-    def test_parse_records_success(self, mockUpdates, mockClient):
-        indexRecords()
-        mockClient.assert_called_once()
-        mockUpdates.assert_called_once()
+    @patch.multiple(SessionManager, createSession=DEFAULT, closeConnection=DEFAULT)
+    def test_parse_records_success(self, mockClient, createSession, closeConnection):
+        with patch.object(self.indexingManager, 'loadUpdates') as mockUpdates:
+            self.indexRecords()
+            createSession.assert_called_once()
+            mockClient.assert_called_once()
+            mockUpdates.assert_called_once()
+            closeConnection.assert_called_once()
